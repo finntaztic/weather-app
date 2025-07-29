936b331472f5bd59f5e785aae0b59a91237c0679
finntastic <179311210+finntaztic@users.noreply.github.com>
2025-07-29 08:14:39 +0800

    Fetch data

--- index.js
+++ index.js
@@ -1,13 +1,23 @@
+fetchWeather();
 async function fetchWeather(){
     try {
-        let response = await fetch ('https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/san%20miguel%2C%20catanduanes?unitGroup=metric&key=VDC7PX7MEU4NV29HN2GF88A37&contentType=json');
-        let data = await response.json();
-        console.log(data.address);
+        const response = await fetch ('https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/pasay/today?unitGroup=metric&include=days&key=VDC7PX7MEU4NV29HN2GF88A37&contentType=json');
+        
+        if (!response.ok){
+            throw new Error ('Could not fetch resource');
+        }
+
+        const data = await response.json();
         console.log(data);
-        console.log(data.days);
+        console.log(data.resolvedAddress);//address
+        console.log(data.days[0].icon); //icon
+        console.log(data.days[0].description); //description
+        console.log(data.days[0].feelslike); //temparature
+
+
     } catch (error) {
-        console.error('failed to fetch weather', error)
+        console.error(error)
     }
 };
 
-fetchWeather();
+
