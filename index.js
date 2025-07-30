7757de27a19c79aa44256b26a6a46fa3a518dbe3
finntastic <179311210+finntaztic@users.noreply.github.com>
2025-07-31 07:55:54 +0800

    fix the loc

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
 
+let initialLocation = 'pasay';
+let initialUnit = 'metric';
 
+fetchWeather(initialLocation, initialUnit);
 
-async function fetchWeather(location){
+async function fetchWeather(location, unit = 'metric'){
+
     try {
-
-        const response = await fetch (`https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${location}/today?unitGroup=metric&include=days&key=VDC7PX7MEU4NV29HN2GF88A37&contentType=json`);
+        const response = await fetch (`https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${location}/today?unitGroup=${unit}&include=days&key=VDC7PX7MEU4NV29HN2GF88A37&contentType=json`);
         
         //if search btn aint clicked yet, in
 
@@ -13,25 +16,77 @@ async function fetchWeather(location){
         }
 
         const data = await response.json();
-        console.log(data);
-        console.log(data.resolvedAddress);//address
-        console.log(data.days[0].icon); //icon
-        console.log(data.days[0].description); //description
-        console.log(data.days[0].feelslike); //temparature
+
+        insertWeather(data);
     } catch (error) {
         console.error(error)
     }
 };
 
-fetchWeather('pasay');
+function insertWeather(data){
+    const weatherInfo = document.querySelector ('.weather-desc');
+    weatherInfo.innerHTML = '';
 
+    const address = document.createElement('p');
+    address.textContent = data.resolvedAddress;
+    weatherInfo.appendChild(address);
+
+    //should be image 
+    const icon = document.createElement('p');
+    icon.textContent = data.days[0].icon;
+    weatherInfo.appendChild(icon);
+
+    const description = document.createElement('p');
+    description.textContent = data.days[0].description;
+    weatherInfo.appendChild(description);
+
+    const temparature = document.createElement('p');
+    temparature.textContent = data.days[0].feelslike;
+    weatherInfo.appendChild(temparature);
+};
 //change location based on inputs
 const searchBtn = document.querySelector('#searchBtn');
 searchBtn.addEventListener('click', () => {
     const newLocation = document.getElementById('search').value.toLowerCase();
-    fetchWeather(newLocation);
+    const toggle = document.querySelector('.toggle');
+     
+    if (newLocation){
+        initialLocation = newLocation;
+        fetchWeather(newLocation);
+    } else {
+        alert("Please enter a new location");
+    }
+
+    //changes the temperature unit on click
+    let clickCount = 0;
+    toggle.addEventListener('click', () => {
+            clickCount++
+            console.log(clickCount);
+
+        if (clickCount % 2 !== 0){
+            fetchWeather(newLocation, unit = 'us');
+        } else if (clickCount % 2 === 0){
+            fetchWeather(newLocation, unit = 'metric');
+        }
+    })
 });
 
 //change temperature unit of measure
 
+const toggle = document.querySelector('.toggle');
+   let clickCount = 0;
+    toggle.addEventListener('click', () => {
+            clickCount++
+            console.log(clickCount);
 
+        if (clickCount % 2 !== 0){
+            fetchWeather(initialLocation, initialUnit = 'us')
+        } else if (clickCount % 2 === 0){
+            fetchWeather(initialLocation, initialUnit = 'metric');
+        }
+    })
+
+toggle.addEventListener('click', () => {
+    initialUnit = 'us';
+    fetchWeather(initialLocation, initialUnit)
+})
